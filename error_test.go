395114ff9b65ee6045209547fedb42f5df527481
Rashaad R. Randall <hello@rashaadrandall.com>
2022-08-04 15:33:50 -0500

    test: error unit test

---
+++ error_test.go
@@ -0,0 +1,30 @@
+package safeguard
+
+import (
+	"testing"
+)
+
+func TestError(t *testing.T) {
+	tests := []struct {
+		name string
+		arg string
+	}{
+		{
+			name: "Test 'Hello World'",
+			arg: "Hello World",
+		},
+		{
+			name: "Test 'foobar'",
+			arg: "foobar",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := Error(tt.arg)
+			if err.Error() != tt.arg {
+				t.Error("Error() failed")
+			}
+		})
+	}
+}
